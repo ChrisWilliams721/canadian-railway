710b87e3d88efd6fa6706bc0c902164575996cfa
Chris Williams <cchriswilliams721@gmail.com>
2024-12-10 11:18:44 -0700

    UserActivity

---
+++ src/app/Models/UserActivity.jsx
@@ -0,0 +1,40 @@
+import Account from "./Account";
+import WeatherStation from "./WeatherStation";
+
+export default class UserActivity {
+  constructor(
+    activityId,
+    userId,
+    timestamp,
+    description,
+    location,
+    status,
+    activityType,
+  ) {
+    this.activityId = activityId;
+    this.userId = userId;
+    this.timestamp = timestamp;
+    this.description = description;
+    this.location = location;
+    this.status = status;
+    this.activityType = activityType;
+  }
+  // Methods
+  logActivity() {
+    // Add the activity to the user's activity log
+    // =Store it in a database
+  }
+
+  getUserActivity() {
+    // Get the user's activity log
+    // =Retrieve it from the database
+  }
+  filterActivity() {
+    // Filter the user's activity log based on a specific criteria
+    // =Retrieve it from the database
+  }
+  generateActivityReport() {
+    // Generate a report of the user's activity log
+    // =Retrieve it from the database
+  }
+}
